d4b8a5324faa1eac6402a8085d51fc4a1797d458
Kai Brune <kai@brunemac.local>
2018-06-12 01:39:00 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# darkmode
+Change the stylesheet easy and quick depending on the time of day.
+
---
+++ js/darkmode.js
@@ -0,0 +1,36 @@
+window.onload = changeTheme();
+
+function changeTheme() {
+  const sunrisehour = 6; // describes the time where the light theme is displayed
+  const sunsethour = 20; // describes the time where the dark theme is displayed
+
+  var currenttime = new Date();
+  var currenthour = (currenttime.getHours());
+    if (currenthour >= sunrisehour && currenthour < sunsethour) {
+      lightTheme();
+    } else {
+      darkTheme();
+    }
+}
+
+// embed the stylesheets in <head>
+
+function darkTheme() {
+  var s1 = document.createElement("link");
+  s1.type = "text/css";
+  s1.rel = "stylesheet";
+  s1.type = "text/css";
+  s1.href = "css/style_dark.css"; // change your stylesheet
+  document.getElementsByTagName('head')[0].appendChild(s1);
+  console.log("darktheme");
+}
+
+function lightTheme() {
+var s1 = document.createElement("link");
+s1.type = "text/css";
+s1.rel = "stylesheet";
+s1.type = "text/css";
+s1.href = "css/style.css"; // change your stylesheet
+document.getElementsByTagName('head')[0].appendChild(s1);
+console.log("lighttheme");
+}
